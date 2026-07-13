fbcbea72ceea55321af1c9ac5e46877814c05e56
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2026-07-12 19:15:57 -0600

    Fix failing checks

--- crates/gremlins-core/src/python/proc.rs
+++ crates/gremlins-core/src/python/proc.rs
@@ -7,13 +7,11 @@ use crate::core::proc;
 
 #[pyfunction]
 pub fn run_ok(cmd: Vec<String>, cwd: Option<PathBuf>) -> PyResult<bool> {
-    proc::run_ok(&cmd, cwd.as_deref()).map_err(|e| {
-        match e.raw_os_error() {
-            Some(_) if e.kind() == std::io::ErrorKind::NotFound => {
-                PyFileNotFoundError::new_err(e.to_string())
-            }
-            Some(errno) => PyOSError::new_err((errno, e.to_string())),
-            None => PyOSError::new_err(e.to_string()),
+    proc::run_ok(&cmd, cwd.as_deref()).map_err(|e| match e.raw_os_error() {
+        Some(_) if e.kind() == std::io::ErrorKind::NotFound => {
+            PyFileNotFoundError::new_err(e.to_string())
         }
+        Some(errno) => PyOSError::new_err((errno, e.to_string())),
+        None => PyOSError::new_err(e.to_string()),
     })
 }
